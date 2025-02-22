6e491e99c8b9735fb9cdf63fef837eceb06f07fc
Paulo Roberto Mesquita <pauloroberto@ajtec.eu>
2025-02-22 18:52:33 -0300

    first commit

---
+++ src/server.ts
@@ -0,0 +1,36 @@
+import fastifyCors from "@fastify/cors";
+import { fastify } from "fastify";
+import { jsonSchemaTransform, serializerCompiler, validatorCompiler, ZodTypeProvider } from "fastify-type-provider-zod";
+import { fastifySwagger } from '@fastify/swagger'
+import { fastifySwaggerUi } from '@fastify/swagger-ui'
+import { subscribeToEventRoute } from "./routes/subscribe-to-event-routes";
+
+
+const app = fastify().withTypeProvider<ZodTypeProvider>();
+
+app.setSerializerCompiler(serializerCompiler);
+app.setValidatorCompiler(validatorCompiler);
+
+app.register(fastifyCors, {
+    origin: true
+});
+
+app.register(fastifySwagger, {
+    openapi: {
+        info: {
+            title: 'NLW Connect',
+            version: '0.0.1'
+        }
+    },
+    transform: jsonSchemaTransform,
+});
+
+app.register(fastifySwaggerUi, {
+    routePrefix: '/docs'
+});
+
+app.register(subscribeToEventRoute)
+
+app.listen({ port: 3333 }).then(() => {
+    console.log('HTTP server running!')
+})
