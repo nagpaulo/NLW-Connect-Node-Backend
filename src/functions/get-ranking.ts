4e1809711678ef17dc73a8b646a2f391ac7c3a58
Paulo Roberto Mesquita <pauloroberto@ajtec.eu>
2025-02-23 00:55:00 -0300

    finalizando routes

---
+++ src/functions/get-ranking.ts
@@ -0,0 +1,32 @@
+import { inArray } from 'drizzle-orm'
+import { db } from '../drizzle/client'
+import { subscriptions } from '../drizzle/schema/subscriptions'
+import { redis } from '../redis/client'
+
+export async function getRanking() {
+  const ranking = await redis.zrevrange('referral:ranking', 0, 2, 'WITHSCORES')
+  const subcriberIdAndScore: Record<string, number> = {}
+
+  for (let i = 0; i < ranking.length; i += 2) {
+    subcriberIdAndScore[ranking[i]] = Number.parseInt(ranking[i + 1])
+  }
+
+  const subscribers = await db
+    .select()
+    .from(subscriptions)
+    .where(inArray(subscriptions.id, Object.keys(subcriberIdAndScore)))
+
+  const rankingWithScore = subscribers
+    .map(subscriber => {
+      return {
+        id: subscriber.id,
+        name: subscriber.name,
+        score: subcriberIdAndScore[subscriber.id],
+      }
+    })
+    .sort((sub1, sub2) => {
+      return sub2.score - sub1.score
+    })
+
+  return { rankingWithScore }
+}
